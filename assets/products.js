157833a48b1ab485d4bffbe9ba6bf8c4a5761934
Martin Roa <endock@hotmail.com>
2022-10-24 01:28:38 -0300

    se crearon los productos y el carrito de compras con sus estilos

---
+++ assets/products.js
@@ -0,0 +1,20 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Rubia",
+        precio: 350,
+        img: "./img/rubia.jpg"
+    },
+    {
+        id: 2,
+        nombre: "Roja",
+        precio: 400,
+        img: "./img/roja.png"
+    },
+    {
+        id: 3,
+        nombre: "Negra",
+        precio: 450,
+        img: "./img/negra.jpg"
+    },
+]
