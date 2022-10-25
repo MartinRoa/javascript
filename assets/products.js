ed0d2ecd4fcaab6c2213b44f0f508efe03ce4ef4
Martin Roa <endock@hotmail.com>
2022-10-25 07:16:09 -0300

    se agrego cantidad, eliminar y botones al carrito

--- assets/products.js
+++ assets/products.js
@@ -3,18 +3,21 @@ const productos = [
         id: 1,
         nombre: "Rubia",
         precio: 350,
-        img: "./img/rubia.jpg"
+        img: "./img/rubia.jpg",
+        cantidad: 1,
     },
     {
         id: 2,
         nombre: "Roja",
         precio: 400,
-        img: "./img/roja.png"
+        img: "./img/roja.png",
+        cantidad: 1,
     },
     {
         id: 3,
         nombre: "Negra",
         precio: 450,
-        img: "./img/negra.jpg"
+        img: "./img/negra.jpg",
+        cantidad: 1,
     },
 ]
--- assets/proyecto.js
+++ assets/proyecto.js
@@ -144,6 +144,7 @@ if (inicio == true) {
 const shopContent = document.getElementById("shopContent");
 const verCarrito = document.getElementById("verCarrito");
 const modalContainer = document.getElementById("modal-container");
+const cantidadCarrito = document.getElementById("cantidadCarrito");
 
 let carrito = [];
 
@@ -165,52 +166,25 @@ productos.forEach((product)=>{
     content.append(comprar);
 
     comprar.addEventListener("click", () =>{
+
+    const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
+    
+    if (repeat) {
+        carrito.map((prod) => {
+            if(prod.id === product.id) {
+                prod.cantidad++;
+            }
+        });
+    }else {
         carrito.push({
             id : product.id,
             img: product.img,
             nombre: product.nombre,
             precio: product.precio,
+            cantidad: product.cantidad,
         });
+    }
         console.log(carrito);
+        carritoCounter();
     });
-});
-
-verCarrito.addEventListener("click", () => {
-    modalContainer.innerHTML = "";
-    modalContainer.style.display = "flex";
-    const modalHeader = document.createElement("div");
-    modalHeader.className = "modal-header";
-    modalHeader.innerHTML = `
-    <h1 class="modal-header-title">Carrito.</h1>
-    `;
-    modalContainer.append(modalHeader);
-
-    const modalbutton = document.createElement("h1");
-    modalbutton.innerText = "x";
-    modalbutton.className = "modal-header-button";
-
-    modalbutton.addEventListener("click", () => {
-        modalContainer.style.display = "none";
-    });
-
-    modalHeader.append(modalbutton);
-
-    carrito.forEach((product) => {
-        let carritoContent = document.createElement("div");
-        carritoContent.className = "modal-content"
-        carritoContent.innerHTML = `
-            <img src="${product.img}">
-            <h3>${product.nombre}</h3>
-            <p>${product.precio} $</p>
-        `;
-
-        modalContainer.append(carritoContent)
-    });
-
-    const total = carrito.reduce((acc, el) => acc + el.precio, 0);
-
-    const totalBuying = document.createElement("div");
-    totalBuying.className = "total-content";
-    totalBuying.innerHTML = `total a pagar: ${total} $`;
-    modalContainer.append(totalBuying);
 });
